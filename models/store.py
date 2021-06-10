59b86cec4cbe8220dabf97da1871ff2264a67035
SMN48 <smn48@pitt.edu>
2021-06-10 08:01:19 -0400

    added files necessary for Heroku

--- models/store.py
+++ models/store.py
@@ -2,7 +2,7 @@ from db import db
 
 
 class StoreModel(db.Model):
-    __tablename__ = 'store'
+    __tablename__ = 'stores'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
